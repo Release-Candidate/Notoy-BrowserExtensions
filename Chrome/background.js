245c5c08b9ec3ef5b34671a999e88e234a4e090a
Release-Candidate <rec@gmx.at>
2021-08-10 13:11:41 +0200

    Add files

---
+++ Chrome/background.js
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Roland Csaszar
+//
+// Project:  notoy-browser_extensions
+// File:     background.js
+// Date:     10.Aug.2021
+//
+//=============================================================================
