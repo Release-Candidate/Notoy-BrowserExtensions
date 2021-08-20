d355c5354adaa6269572166c30d02df6a2f473c7
Release-Candidate <rec@gmx.at>
2021-08-20 13:10:53 +0200

    Add Firefox files and config

---
+++ Firefox/background.js
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Roland Csaszar
+//
+// Project:  notoy-browser_extensions
+// File:     background.js
+// Date:     20.Aug.2021
+//
+//=============================================================================
+
+let tabTitle = "Title"
+let tabUrl = "about:blank"
+
+chrome.runtime.onInstalled.addListener(() => {
+    // Init goes here
+})
+
+chrome.tabs.onActivated.addListener(() => {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, ([currTab]) => {
+        tabTitle = currTab.title
+        tabUrl = currTab.url
+
+        chrome.storage.sync.set({ tabUrl })
+        chrome.storage.sync.set({ tabTitle })
+    })
+})
